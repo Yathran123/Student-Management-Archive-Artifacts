35f598bc50365ff8d77b5d83ad13dcaef389582b
Yathran123 <yathran.r@vitstudent.ac.in>
2026-09-17 12:38:10 +0530

    Added report generation

---
+++ app.py
@@ -0,0 +1,7 @@
+print("Student Management Project 2")
+
+with open("report.txt", "w") as file:
+    file.write("Student Management Build Report\n")
+    file.write("Build completed successfully\n")
+
+print("report.txt created successfully")
